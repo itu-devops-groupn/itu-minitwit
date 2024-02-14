2be51fd54306b116608f972395427ab53c1331d1
Daniel Millard <dmil@itu.dk>
2024-02-14 11:34:29 +0100

    Add content to public
    Add record of messages
    Public displays all messages if any
    Public Page model has no way of populating the messages list
    
    Co-authored-by: Frederik <frlr@itu.dk>
    Co-authored-by: Jacob <jacp@itu.dk>
    Co-authored-by: Rasmus <raln@itu.dk>

--- src/itu-minitwit/Pages/Public.cshtml.cs
+++ src/itu-minitwit/Pages/Public.cshtml.cs
@@ -1,13 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace itu_minitwit.Pages
+namespace itu_minitwit.Pages;
+
+public class PublicModel : PageModel
 {
-    public class PublicModel : PageModel
+    public List<Message> Messages = new();
+
+    public IActionResult OnGet()
     {
-        public IActionResult OnGet()
-        {
-            return Page();
-        }
+        
+        return Page();
     }
+
+}
+
+public record Message
+{
+    public string Text { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Date { get; set; }
+    public string GravatarUrl { get; set; }
 }
