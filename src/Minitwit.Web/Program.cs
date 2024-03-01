fa5d1cbbdb58c255e46ef3be6fa1c7cc780b2b73
Jacob Pærregaard <jacp@itu.dk>
2024-03-01 12:47:42 +0100

    fixed stuff

--- src/Minitwit.Web/Program.cs
+++ src/Minitwit.Web/Program.cs
@@ -33,13 +33,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-if (!File.Exists("/tmp/minitwit.db"))
-{
-    var context = app.Services.GetRequiredService<MinitwitContext>();
-    context.Database.EnsureCreated();
-    context.Database.ExecuteSqlRaw(File.ReadAllText("/tmp/schema.sql"));
-}
-
 app.UseEndpoints(endpoints =>
         {
             _ = endpoints.MapRazorPages(); // Map Razor Pages
