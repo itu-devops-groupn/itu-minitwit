cb368485d003399f6d6651261f3d66f96730926d
Jacob Pærregaard <jacp@itu.dk>
2024-04-26 17:13:20 +0200

    fix spelling and enable workflow

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -3,7 +3,7 @@
 tag=$1
 
 docker pull dsmillard/minitwit-web:$tag
-docekr pull dsmillard/minitwit-api:$tag
+docker pull dsmillard/minitwit-api:$tag
 
 docker service update --image dsmillard/minitwit-web:$tag vagrant_minitwit-web-image
 docker service update --image dsmillard/minitwit-api:$tag vagrant_minitwit-api-image
