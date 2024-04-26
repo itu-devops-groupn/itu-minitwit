9c377bde64c12c4a6df954697ca2f7db7c344a17
Jacob Pærregaard <jacp@itu.dk>
2024-04-26 16:54:09 +0200

    update remote files

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -1,6 +1,12 @@
-source ~/.bashrc
+#!/bin/bash
 
-cd /vagrant
+tag=$1
 
-docker compose -f docker-compose.yml pull
-docker compose -f docker-compose.yml up -d
+docker pull dsmillard/minitwit-web:$tag
+docekr pull dsmillard/minitwit-api:$tag
+
+docker service update --image dsmillard/minitwit-web:$tag vagrant_minitwit-web-image
+docker service update --image dsmillard/minitwit-api:$tag vagrant_minitwit-api-image
+
+docker service update --image grafana/grafana vagrant_grafana
+docker service update --image prom/prometheus vagrant_prometheus
