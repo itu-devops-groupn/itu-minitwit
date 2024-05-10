b098ee55a7c1206575f37b9b1bff8a8167d2ee0a
Natalie Clara Petersen <natp@itu.dk>
2024-05-11 01:29:59 +0200

    deploy script also deploys first time

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-tag=$1
-
 docker compose pull
 
-for service in $(docker service ls --format "{{.Name}}")
-do
-    # Extract the image name from the service
-    image=$(docker service inspect --format '{{ .Spec.TaskTemplate.ContainerSpec.Image }}' $service | cut -d ":" -f1)
+service_count=$(docker service ls --format "{{.Name}}" | wc -l)
 
-    # Update the service with the latest tag
-    docker service update --image $image:latest $service
-done
+# If no services are running, deploy the stack
+if [ "$service_count" -eq 0 ]; then
+    docker stack deploy -c docker-compose.yml group_n
+else
+    for service in $(docker service ls --format "{{.Name}}"); do
+        # Extract the image name from the service
+        image=$(docker service inspect --format '{{ .Spec.TaskTemplate.ContainerSpec.Image }}' $service | cut -d ":" -f1)
+
+        # Update the service with the latest tag
+        docker service update --image $image:latest $service
+    done
+fi
